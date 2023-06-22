082b8792b6db587e3444d182e9265c4147dde551
unknown <fhaourigui1@gmail.com>
2023-06-22 12:48:37 +0100

    first try

---
+++ script.js
@@ -0,0 +1,21 @@
+//jshint eversion: 6
+
+const express = require ("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+
+const app = express();
+
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/test.html")
+})
+
+app.listen(4000, function(){
+    console.log("server is running on port 4000")
+})
